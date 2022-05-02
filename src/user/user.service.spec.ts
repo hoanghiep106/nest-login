0fc365b851b592f79886ac6f15f40864bf7e4ced
Hiep Nguyen <hoanghiepnguyen106@gmail.com>
2022-05-02 20:35:15 +0700

    test: add tests for user service

---
+++ src/user/user.service.spec.ts
@@ -0,0 +1,59 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as passwordUtils from '../utils/password';
+import { User } from './user.schema';
+import { UserService } from './user.service';
+
+export type MockModelType = {
+  save: jest.Mock<any, any>;
+};
+
+const mockModelInstance = {
+  save: jest.fn(),
+};
+
+export const mockModel = jest.fn(() => mockModelInstance);
+
+describe('UserService', () => {
+  let userService: UserService;
+  let mockUserModel: MockModelType;
+
+  const mockUsername = 'hiep';
+  const mockPlainPassword = 'ah56XGG@*Lp^wgAr';
+  const mockHashedPassword =
+    '$2b$10$cjI.PiVqLrVfkWPWhNibseEwAH3ZCPMmtcIdQJodNOrW/Ha1A8zTS';
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getModelToken(User.name),
+          useValue: mockModel,
+        },
+      ],
+    }).compile();
+
+    userService = module.get<UserService>(UserService);
+    mockUserModel = module.get(getModelToken(User.name));
+  });
+
+  it('should be defined', () => {
+    expect(userService).toBeDefined();
+    expect(mockUserModel).toBeDefined();
+  });
+
+  it('user should be saved with hashed password', async () => {
+    jest
+      .spyOn(passwordUtils, 'hashPassword')
+      .mockReturnValueOnce(Promise.resolve(mockHashedPassword));
+
+    await userService.create({ username: mockUsername }, mockPlainPassword);
+
+    expect(mockUserModel).toBeCalledWith({
+      username: mockUsername,
+      hashedPassword: mockHashedPassword,
+    });
+    expect(mockModelInstance.save).toBeCalled();
+  });
+});
